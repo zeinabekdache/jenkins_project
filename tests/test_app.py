67c8e43c4001fd4d53fefec501041a06a5d11f09
Zeina Bekdache <zeinabekdache@Zeinas-MacBook-Air.local>
2024-12-09 23:32:51 +0200

    Fix linter issues (W293, W292) in app.py

--- tests/test_app.py
+++ tests/test_app.py
@@ -6,5 +6,6 @@ class TestApp(unittest.TestCase):
     def test_greet(self):
         self.assertEqual(greet("World"), "Hello, World from Zeina Bekdache!")
 
+
 if __name__ == "__main__":
     unittest.main()
