99cbddb7db05f5b71212c38921931bc0f5a7768c
Zeina Bekdache <zeinabekdache@Zeinas-MacBook-Air.local>
2024-12-09 23:02:31 +0200

    lab 10 done jenkins pipelines

---
+++ app.py
@@ -0,0 +1,7 @@
+def greet(name):
+   
+    return f"Hello, {name} from Zeina Bekdache!"
+
+
+if __name__ == "__main__":
+    print(greet("World"))
---
+++ tests/test_app.py
@@ -0,0 +1,10 @@
+import unittest
+from app import greet
+
+
+class TestApp(unittest.TestCase):
+    def test_greet(self):
+        self.assertEqual(greet("World"), "Hello, World from Zeina Bekdache!")
+
+if __name__ == "__main__":
+    unittest.main()
